b31183fe7802da291a9228ad2555aab104d7079c
fletcherist <romanovexports@gmail.com>
2017-12-09 13:18:10 +0300

    [feature]: applying compose

--- src/index.js
+++ src/index.js
@@ -9,6 +9,10 @@ const ALPHABET = {
   }
 }
 
+// fp composition & pipe helpers
+const pipe = (fn, ...fns) => (...args) => fns.reduce((result, fn) => fn(result), fn(...args))
+const compose = (...fns) => (...args) => pipe(...fns.reverse())(...args)
+
 class Player {
   isPlaying: boolean = false
   play () { this.isPlaying = true }
@@ -18,17 +22,24 @@ class Player {
 
 class Speaker extends Player {
   synth = window.speechSynthesis
+  currentUtterance: Object
   constructor () {
     super()
     this.synth.onvoicechanged = event => console.log(event)
     this.synth.onvoiceschanged = event => console.log(event)
   }
   speak (utter) {
-    this.synth.speak(utter)
+    this.currentUtterance = utter || this.currentUtterance
+    this.currentUtterance.rate = this.currentUtterance.rate + 0.1
+    this.synth.speak(this.currentUtterance)
     console.log(this.synth)
   }
   pause () { this.synth.pause() }
   resume () { this.synth.resume() }
+  setSpeed (value: number) {
+    // this.currentUtterance.rate = value
+    // this.speak()
+  }
 }
 
 const app = {
@@ -40,23 +51,22 @@ const app = {
   speaker: new Speaker()
 }
 
-input.addEventListener('paste', (event: Event) => {
-  console.log(event)
-  const text = event.target.value
-
-})
-
 /*
  * Analyses the first letter in the word
  * Now it can guess between cyrilic and latin letter only
  */
 const detectLangByStr = (str: string) => {
-  const firstCharCode = str.toLowerCase().charCodeAt(0)
-  for (let alphabet in ALPHABET) {
-    if (firstCharCode >= ALPHABET[alphabet].unicode[0] &&
-        firstCharCode <= ALPHABET[alphabet].unicode[1]) {
-      return alphabet
+  let currentCharIndex = 0
+  let maxCharIndex = 3
+  while (currentCharIndex <= maxCharIndex) {
+    const charCode = str.toLowerCase().charCodeAt(currentCharIndex)
+    for (let alphabet in ALPHABET) {
+      if (charCode >= ALPHABET[alphabet].unicode[0] &&
+          charCode <= ALPHABET[alphabet].unicode[1]) {
+        return alphabet
+      }
     }
+    currentCharIndex++
   }
   return 'en'
 }
@@ -71,7 +81,7 @@ const isTheSameLanguage = (
   word2: wordType
 ) => word1.lang === word2.lang
 
-const joinOneLanguageWords = (words: Array<Object>) => {
+const joinOneLanguageWords = (words: Array<wordType>) => {
   const sentences = []
   words.forEach(word => {
     if (sentences.length === 0) return sentences.push(word)
@@ -80,27 +90,34 @@ const joinOneLanguageWords = (words: Array<Object>) => {
           [sentences[sentences.length - 1].token, word.token].join(' ')
       : sentences.push(word)
   })
-  console.log(sentences)
   return sentences
 }
 
-const splitTextIntoSentences = (text: string): Array => text.split('.')
-const splitSentenceIntoWords = (sentence: string): Array => sentence.split(' ')
+const splitTextIntoSentences = (text: string): Array<string> => text.split('.')
+const splitSentenceIntoWords = (sentence: string): Array<string> => sentence.split(' ')
+const convertWordsIntoTokens = (words: Array<string>): Array<wordType> =>
+  words.map((token: string) => ({
+    lang: detectLangByStr(token),
+    token: token
+  }))
+const filterWordsArray = (words: Array<wordType>) =>
+  words.filter(word => word.token.length !== 0)
 
-const text = input.value
+const text = input.value.trim()
 const sentences = splitTextIntoSentences(text)
 sentences.forEach(sentence => {
-  const textTokens = splitSentenceIntoWords(sentence)
-    .map((token: string) => ({
-      lang: detectLangByStr(token),
-      token: token
-    }))
+  let textTokens = compose(
+    filterWordsArray,
+    convertWordsIntoTokens,
+    splitSentenceIntoWords
+  )(text)
+  console.log(textTokens)
 
   const speakEvents = joinOneLanguageWords(textTokens).map(
     sentence => {
       const utterThis = new SpeechSynthesisUtterance(sentence.token)
       utterThis.lang = sentence.lang
-      utterThis.rate = 1.6
+      utterThis.rate = 1.9
       return utterThis
     }
   )
@@ -110,6 +127,12 @@ sentences.forEach(sentence => {
   })
 })
 
+// let current = 1
+// setInterval(() => {
+//   app.speaker.setSpeed(current)
+//   current += .2
+// }, 1000)
+
 // document.addEventListener('keydown', (event: Event) => {
 //   // If space is pressed
 //   if (event.keyCode === 32) {
@@ -118,3 +141,8 @@ sentences.forEach(sentence => {
 //   console.log(event.keyCode)
 // })
 // button.addEventListener('click', (event) => {})
+
+// input.addEventListener('paste', (event: Event) => {
+//   console.log(event)
+//   const text = event.target.value
+// })
